7e2acc13f034760cfca56ca41c2b3d4ffc15f698
PotapenkoVadim <potapenko.v00@mail.ru>
2021-07-19 22:29:00 +0300

    implement infinite scroll

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import { RefObject } from 'react';
+export declare function useInfiniteScrollHook(refContainer: RefObject<HTMLElement>, onLoad: Function): void;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RefObject, useEffect } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export function useInfiniteScrollHook(refContainer: RefObject<HTMLElement>, onLoad: Function): void {
+  useEffect(() => {
+    if (refContainer?.current && onLoad) {
+      const handleScroll = (): void => {
+        const { scrollHeight, clientHeight, scrollTop } = refContainer.current!;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        const containerScrollTop = scrollHeight - clientHeight;
+        const currentScrollPosition = Math.ceil(Math.abs(scrollTop));
+
+        if (currentScrollPosition >= containerScrollTop) {
+          onLoad();
+        }
+      };
+
+      refContainer.current!.addEventListener('scroll', handleScroll);
+
+      return () => refContainer.current!.removeEventListener('scroll', handleScroll);
+    }
+  }, [refContainer, onLoad]);
+}
