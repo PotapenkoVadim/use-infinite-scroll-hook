d7fade14e6a02c95cc5a63474b42bdbdeefb3a1f
PotapenkoVadim <potapenko.v00@mail.ru>
2021-07-21 21:44:47 +0300

    add gap

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { RefObject, useEffect } from 'react';
 
-export function useInfiniteScrollHook(refContainer: RefObject<HTMLElement>, onLoad: Function): void {
+export function useInfiniteScrollHook(
+  refContainer: RefObject<HTMLElement>,
+  onLoad: Function
+): void {
   useEffect(() => {
     if (refContainer?.current && onLoad) {
       const handleScroll = (): void => {
         const { scrollHeight, clientHeight, scrollTop } = refContainer.current!;
 
-        const containerScrollTop = scrollHeight - clientHeight;
+        const containerScrollTop = scrollHeight - clientHeight - 5;
         const currentScrollPosition = Math.ceil(Math.abs(scrollTop));
 
         if (currentScrollPosition >= containerScrollTop) {
